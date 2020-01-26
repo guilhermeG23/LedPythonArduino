de60f35a3bdf59145c60f674578eada96d10126d
ban <guilhermebrechot@gmail.com>
2020-01-26 14:45:07 -0300

    Arrumando um pouco

--- PyLEDArduino.py
+++ PyLEDArduino.py
@@ -3,19 +3,20 @@
 import serial
 import time
 
-conexao = serial.Serial('COM3', 9600)
+conexao = serial.Serial('COM6', 9600)
+
 print('Acender o LED')
 if conexao.isOpen() == True:
     def tempoL():
         time.sleep(1)
-        conexao.write('1')
+        conexao.write(str(1))
     def tempoD():
         time.sleep(1)
-        conexao.write('2')
+        conexao.write(str(2))
     while True:
         tempoL()
         tempoD()
 else:
-    print('Não Funcionou')
+    print('Nao Funcionou')
         
     
